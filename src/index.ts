f982193c24fe9012223e743340808cab0d6d5d3d
Jacob Evans <cloud887@gmail.com>
2020-12-07 23:46:45 -0600

    added linter

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,22 @@
 import { Probot } from "probot";
 
 export = ({ app }: { app: Probot }) => {
-  app.on("issues.opened", async (context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
-    });
-    await context.octokit.issues.createComment(issueComment);
+  app.on("issue_comment.created", async (context) => {
+    const comment = context.payload.comment;
+    const analyzeCmd = comment.body.search("!analyze");
+    if (!analyzeCmd) return;
+    
+    // const issueComment = context.issue({
+    //   body: "Thanks for opening this issue!",
+    // });
+    // await context.octokit.issues.createComment(issueComment);
   });
-  // For more information on building apps:
-  // https://probot.github.io/docs/
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+  // app.on("pull_request_review_comment.created", async (context) => {
+  //   console.log(context);
+  //   // const issueComment = context.issue({
+  //   //   body: "Thanks for opening this issue!",
+  //   // });
+  //   // await context.octokit.issues.createComment(issueComment);
+  // });
 };
