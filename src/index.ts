8e84e8746814c4e40660679c7b905c5d5c1d492a
Jacob Evans <cloud887@gmail.com>
2020-12-08 00:25:54 -0600

    Linter

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
 import { Probot } from "probot";
 
 export = ({ app }: { app: Probot }) => {
-  app.on("issue_comment.created", async (context) => {
+  app.on(`issue_comment.created`, async (context) => {
     const comment = context.payload.comment;
-    const analyzeCmd = comment.body.search("!analyze");
+    const analyzeCmd = comment.body.search(`!analyze`);
     if (!analyzeCmd) return;
-    
+    console.log(comment.body);
     // const issueComment = context.issue({
     //   body: "Thanks for opening this issue!",
     // });
     // await context.octokit.issues.createComment(issueComment);
+    return;
   });
 
   // app.on("pull_request_review_comment.created", async (context) => {
