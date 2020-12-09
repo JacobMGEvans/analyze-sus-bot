f22034f63894aa4685b73aa5a233fddf88f9370e
Jacob Evans <cloud887@gmail.com>
2020-12-08 23:44:06 -0600

    analysis call completed

--- src/index.ts
+++ src/index.ts
@@ -8,17 +8,27 @@ export = async ({ app }: { app: Probot }): Promise<void> => {
     const bodyArr = comment.body.split(` `);
 
     const analyzeCmd = bodyArr.findIndex((value) => value === `!analyze`);
-    if (!bodyArr[analyzeCmd]) return;
-
-    // const cmdType = bodyArr[analyzeCmd];
-    // const searchType = bodyArr[analyzeCmd + 1];
+    const cmdType = bodyArr[analyzeCmd];
+    const searchType = bodyArr[analyzeCmd + 1];
+    if (!cmdType && !searchType) return;
     const analyzeTarget = bodyArr[analyzeCmd + 2];
-    const response = virusTotalURLReqs(analyzeTarget);
+
+    type VTURLRes = {
+      id: ``;
+      type: ``;
+    } | null;
+
     try {
-      console.log(await response);
+      const resObj: VTURLRes = (await VTURLReq(analyzeTarget)).data;
+      console.log(resObj);
+      if (!resObj?.id) return;
+
+      const analysisRes = await VTIDAnalysis(resObj?.id);
+      console.log(analysisRes.data.attributes.stats);
     } catch (er) {
       console.log(er.response, `ERROR`);
     }
+
     // const issueComment = context.issue({
     //   body: "Thanks for opening this issue!",
     // });
@@ -27,25 +37,27 @@ export = async ({ app }: { app: Probot }): Promise<void> => {
 
   // app.on("pull_request_review_comment.created", async (context) => {
   //   console.log(context);
-  //   // const issueComment = context.issue({
-  //   //   body: "Thanks for opening this issue!",
-  //   // });
-  //   // await context.octokit.issues.createComment(issueComment);
   // });
 };
-// type urlRes = {
-//   data: {
-//     id: ``;
-//     type: ``;
-//   };
-// };
-const virusTotalURLReqs = async (url: string) => {
+
+const VTURLReq = async (url: string) => {
   const formStruct = new URLSearchParams();
   formStruct.append(`url`, url);
   return (
     await fetch(`https://www.virustotal.com/api/v3/urls`, {
       method: `POST`,
       body: formStruct,
+      headers: {
+        "x-apikey": process.env.VIRUS_TOTAL_KEY as string,
+      },
+    })
+  ).json();
+};
+
+const VTIDAnalysis = async (id: string) => {
+  return await (
+    await fetch(`https://www.virustotal.com/api/v3/analyses/${id}`, {
+      method: `GET`,
       headers: {
         "x-apikey": process.env.VIRUS_TOTAL_KEY as string,
       },
